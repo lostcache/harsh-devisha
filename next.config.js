05a4eddc0a96abb5d796763af981b0166377bbe1
Harsh Devisha <harshdevisha9111@gmail.com>
2025-01-27 12:18:47 -0800

    [feat] github workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  output: "export"
+  output: "export",
+  basePath: "/harsh-devisha",
+  assetPrefix: "/harsh-devisha/",
 };
 
 module.exports = nextConfig;
